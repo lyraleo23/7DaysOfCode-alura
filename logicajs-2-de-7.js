188fb4d4e7bfcd03e1e494f618a91f3f781a9ee2
lyraleo23 <lyraleo@gmail.com>
2022-07-05 11:48:24 -0300

    solução desafio 2

--- logicajs-2-de-7.js
+++ logicajs-2-de-7.js
@@ -64,3 +64,49 @@ Teste os códigos acima e tente adaptá-los ao seu programa!
   
 */
 
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
+
+console.log(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
+
+const resposta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`);
+
+if (resposta === 1) {
+    console.log("Muito bom! Continue estudando e você terá muito sucesso.")
+} else {
+    if (resposta === 2) {
+        console.log("Ahh que pena... Já tentou aprender outras linguagens?");
+    } else {
+        console.log("resposta inválida");
+    }
+}
+
+/*
+RESPOSTA DO DESAFIO DO DIA 2
+
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
+const msg = `"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"!`;
+
+alert(msg);
+
+RESPOSTA DO DESAFIO DO DIA 2 COM DESAFIO OPCIONAL
+
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
+const msg = `"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"!`;
+
+alert(msg);
+
+const gosta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 - SIM ou 2 - NÃO`);
+if (gosta == 1){
+    alert("Muito bom! Continue estudando e você terá muito sucesso.");
+}
+if (gosta == 2){
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
+}
+
+*/
